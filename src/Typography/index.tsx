1d52a54779ed3e674daa8af12f9de6fcb5905a60
lebron <lebron.zhu@coho.com.cn>
2022-05-17 15:54:55 +0800

    重置

--- src/Typography/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Button } from 'antd';
-import 'antd/dist/antd.css';
-
-const Typography = () => {
-  return <Button>Typography</Button>;
-};
-
-export default Typography;
